d742350fc068933826cef5fbe7d00ab1a8728680
Roger Skeie <rogerskeie@gmail.com>
2018-11-06 14:58:49 +0100

    Toolbar button that toggles auto mute added.

--- src/bg.js
+++ src/bg.js
@@ -1,18 +1,43 @@
-(function (){
-	browser.storage.local.get().then(result=>{
-		if(result.normalMode===undefined)browser.storage.local.set({normalMode:true,privateMode:true});
-	});
-	browser.tabs.onCreated.addListener(autoMute);
+(function () {
+    browser.storage.local.get().then(result => {
+        if (result.normalMode === undefined) {
+            browser.storage.local.set({normalMode: true, privateMode: true});
+        }
+
+        if (result.autoMute === undefined) {
+            browser.storage.local.set({autoMute: true})
+        }
+    });
+
+    toggleIcon();
+    browser.tabs.onCreated.addListener(autoMute);
+    browser.browserAction.onClicked.addListener(toggleAutoMute);
 })();
 
-function autoMute(tab){
-	browser.storage.local.get().then(result=>{
-		const normalMode  = result.normalMode;
-		const privateMode = result.privateMode;
-		if((tab.incognito&&privateMode)||(!tab.incognito&&normalMode)){
-			browser.tabs.update(tab.id,{
-				muted: true
-			});
-		}
-	});
+function autoMute(tab) {
+    browser.storage.local.get().then(result => {
+        const normalMode = result.normalMode;
+        const privateMode = result.privateMode;
+
+        if (result.autoMute && ((tab.incognito && privateMode) || (!tab.incognito && normalMode))) {
+            browser.tabs.update(tab.id, {
+                muted: true
+            });
+        }
+    });
+}
+
+function toggleAutoMute() {
+    browser.storage.local.get().then(result => {
+        browser.storage.local.set({autoMute: !result.autoMute})
+        toggleIcon();
+    });
+}
+
+function toggleIcon() {
+    browser.storage.local.get().then(result => {
+        chrome.browserAction.setIcon({
+            path: result.autoMute ? 'Icons/icon_muted.svg' : 'Icons/icon_unmuted.svg'
+        });
+    });
 }
--- src/options.js
+++ src/options.js
@@ -1,21 +1,23 @@
-(function(){
-	document.getElementById("title").textContent=browser.i18n.getMessage("options");
-	document.getElementById("label1").textContent=browser.i18n.getMessage("normal");
-	document.getElementById("label2").textContent=browser.i18n.getMessage("private");
-	document.addEventListener('DOMContentLoaded',restoreOptions);
-	document.getElementById("optionsForm").addEventListener("change",saveOptions);
+(function () {
+    document.getElementById('title').textContent = browser.i18n.getMessage('options');
+    document.getElementById('label1').textContent = browser.i18n.getMessage('normal');
+    document.getElementById('label2').textContent = browser.i18n.getMessage('private');
+    document.addEventListener('DOMContentLoaded', restoreOptions);
+    document.getElementById('optionsForm').addEventListener('change', saveOptions);
 })();
 
-function saveOptions(){
-	browser.storage.local.set({
-		normalMode: document.getElementById("normalMode").checked,
-		privateMode:document.getElementById("privateMode").checked
-	});
+function saveOptions() {
+    browser.storage.local.set({
+        normalMode: document.getElementById('normalMode').checked,
+        privateMode: document.getElementById('privateMode').checked,
+        autoMute: document.getElementById('autoMute').checked
+    });
 }
 
-function restoreOptions(){
-	browser.storage.local.get().then(result=>{
-		document.getElementById("normalMode").checked=result.normalMode;
-		document.getElementById("privateMode").checked=result.privateMode;
-	});
+function restoreOptions() {
+    browser.storage.local.get().then(result => {
+        document.getElementById('normalMode').checked = result.normalMode;
+        document.getElementById('privateMode').checked = result.privateMode;
+        document.getElementById('autoMute').checked = result.autoMute;
+    });
 }
