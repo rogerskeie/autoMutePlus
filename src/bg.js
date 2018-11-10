5176b0e43944c4a88f3fb9ec807c9380e407ff42
Roger Skeie <rogerskeie@gmail.com>
2018-11-10 18:29:09 +0100

    Add domain/url from context menu. Improved checking of whitelist and blacklist.

--- src/bg.js
+++ src/bg.js
@@ -11,28 +11,85 @@
         }
     });
 
+    browser.menus.create({
+        id: 'autoMutePlus',
+        title: browser.i18n.getMessage('extensionName')
+    });
+
+    browser.menus.create({
+        id: 'addDomainToWhitelist',
+        parentId: 'autoMutePlus',
+        title: browser.i18n.getMessage('addDomainToWhitelist')
+    });
+
+    browser.menus.create({
+        id: 'addUrlToWhitelist',
+        parentId: 'autoMutePlus',
+        title: browser.i18n.getMessage('addUrlToWhitelist')
+    });
+
+    browser.menus.create({
+        id: 'addDomainToBlacklist',
+        parentId: 'autoMutePlus',
+        title: browser.i18n.getMessage('addDomainToBlacklist')
+    });
+
+    browser.menus.create({
+        id: 'addUrlToBlacklist',
+        parentId: 'autoMutePlus',
+        title: browser.i18n.getMessage('addUrlToBlacklist')
+    });
+
     toggleIcon();
-    browser.tabs.onCreated.addListener(autoMute);
+    browser.tabs.onCreated.addListener(createdListener);
     browser.browserAction.onClicked.addListener(toggleAutoMute);
+    browser.menus.onClicked.addListener((info, tab) => {
+        var url = new URL(tab.url);
+        [action, listType] = info.menuItemId.split('To');
+        addItemToList(escapeRegExp(action === 'addDomain' ? url.hostname : url.href), listType.toLowerCase());
+    });
 })();
+
+function addItemToList(item, listType) {
+    listType = listType.toLowerCase();
+
+    browser.storage.local.get().then(result => {
+        var listContents = result[listType].trim();
+        var keys = {};
+        var needsNewline = listContents !== '' && !listContents.endsWith('\n');
+        keys[listType] = listContents + (needsNewline ? '\n' : '') + item;
+        browser.storage.local.set(keys);
+    });
+}
+
+function createdListener(tab) {
+    if (tab.url === 'about:newtab') {
+        autoMute(tab);
+    } else {
+        browser.tabs.onUpdated.addListener(updatedListener);
+    }
+}
+
+function updatedListener(tabId, changeInfo, tab) {
+    if (changeInfo.status === 'loading' && tab.url !== 'about:blank') {
+        autoMute(tab);
+        browser.tabs.onUpdated.removeListener(updatedListener);
+    }
+}
 
 function autoMute(tab) {
     browser.storage.local.get().then(result => {
         const normalMode = result.normalMode;
         const privateMode = result.privateMode;
+        var whitelisted = listMatchesTab(result.whitelist, tab);
+        var blacklisted = listMatchesTab(result.blacklist, tab);
 
-        browser.tabs.query({currentWindow: true, active: true}).then(function (tabs) {
-            var whitelisted = listMatchesTab(result.whitelist, tabs[0]);
-            var blacklisted = listMatchesTab(result.blacklist, tabs[0]);
-
-            if (
-                blacklisted
-                || (!whitelisted && result.autoMute && ((tab.incognito && privateMode) || (!tab.incognito && normalMode)))
-            ) {
-                browser.tabs.update(tab.id, {muted: true});
-            }
-        });
-
+        if (
+            blacklisted
+            || (!whitelisted && result.autoMute && ((tab.incognito && privateMode) || (!tab.incognito && normalMode)))
+        ) {
+            browser.tabs.update(tab.id, {muted: true});
+        }
     });
 }
 
@@ -72,4 +129,8 @@ function listMatchesTab(listContents, tab) {
     }
 
     return false;
+}
+
+function escapeRegExp(string) {
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
 }
